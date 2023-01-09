d0dd336bc5ce7e97c650a45b3ee26fb4afaab2ce
YogeshPitale <yogesh.pitale@gmail.com>
2023-01-09 12:56:17 +0530

    Fixed the runtime errors after merge

--- src/main/java/com/wf/stp/rbms/config/DroolsConfig.java
+++ src/main/java/com/wf/stp/rbms/config/DroolsConfig.java
@@ -16,10 +16,6 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class DroolsConfig {
 
-    @Value("${RULES_CUSTOMER_RULES_XLSX_URL}")
-    private String RULES_CUSTOMER_RULES_XLSX_URL;
-    @Value("${RULES_CUSTOMER_RULES_DRL_URL}")
-    private String RULES_CUSTOMER_RULES_DRL_URL;
     @Value("${AGENDA_GROUPS_URL}")
     private String AGENDA_GROUPS_URL;
 
@@ -35,8 +31,8 @@ public class DroolsConfig {
     @RefreshScope
     public KieContainer kieContainer() {
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-        kieFileSystem.write(ResourceFactory.newUrlResource(AGENDA_GROUPS_URL));
-        //kieFileSystem.write(ResourceFactory.newClassPathResource(AGENDA_GROUPS_URL_LOCAL));
+        //kieFileSystem.write(ResourceFactory.newUrlResource(AGENDA_GROUPS_URL));
+        kieFileSystem.write(ResourceFactory.newClassPathResource(AGENDA_GROUPS_URL_LOCAL));
         //kieFileSystem.write(ResourceFactory.newFileResource(RULES_CUSTOMER_RULES_XLSX_FTP));
         KieBuilder kb = kieServices.newKieBuilder(kieFileSystem);
         kb.buildAll();
